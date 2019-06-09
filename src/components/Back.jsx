b9b130e271c06768fb068f2235a78abe227fd4a0
Antoine2357 <antoine.jacqmin@live.fr>
2019-06-09 22:04:57 +0200

    functional API

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React from "react";
 import "../cards.css";
 
-class Back extends React.Component{
-    
-    render(){
-        const min = 1;
-        const max = 10;
-        let rand =  min + (Math.random() * (max-min)) ;
+class Back extends React.Component {
+  render() {
+    const min = 1;
+    const max = 10;
+    let rand = min + Math.random() * (max - min);
 
-        return(
-            <div>
-            <p className="energie">Power level : {Math.round(rand)} </p>
-            <h1 className = "characterName"> {this.props.name} </h1>
-            <img className = "charaImg" src={this.props.character} />
-            <p className="description"><br/>{this.props.skills[0]}<br/>{this.props.skills[1]}<br/>{this.props.skills[2]} </p>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <p className="energie">Power level : {this.props.power} </p>
+        <h1 className="characterName"> {this.props.name} </h1>
+        <img className="charaImg" src={this.props.character} />
+        <p className="description">
+          <br />
+          {this.props.skills[0]}
+          <br />
+          {this.props.skills[1]}
+          <br />
+          {this.props.skills[2]}{" "}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Back;
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,68 +1,81 @@
-import React from 'react';
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
-import Front from './Front';
-import Back from './Back';
-import axios from 'axios';
-import { Card, Button, CardImg, CardTitle, CardText, CardDeck,
-    CardSubtitle, CardBody } from 'reactstrap'; 
+import React from "react";
+import Flippy, { FrontSide, BackSide } from "react-flippy";
+import Front from "./Front";
+import Back from "./Back";
+import axios from "axios";
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardDeck,
+  CardSubtitle,
+  CardBody
+} from "reactstrap";
 import "../cards.css";
 
 class Cards extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            eggs: "",
-            character : "",
-            name :"",
-            skills :[],
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      eggs: "",
+      character: "",
+      name: "",
+      skills: [],
+      power: ""
+    };
+  }
 
-    componentDidMount() {
-        axios.get('http://easteregg.wildcodeschool.fr/api/eggs/random')
-            .then(res => {
-                this.setState({
-                    eggs: res.data.image
-                });
-            });
-        axios.get('http://easteregg.wildcodeschool.fr/api/characters/random')
-            .then(res2 => {
-                this.setState({
-                    character: res2.data.image,
-                    name: res2.data.name,
-                    skills: res2.data.skills
-                });
-            });
-    }
+  getRandomInt = max => {
+    return Math.floor(Math.random() * Math.floor(max));
+  };
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5050/characters/${this.getRandomInt(8)}`)
+      .then(res => {
+        this.setState({
+          character: res.data.image,
+          name: res.data.name,
+          skills: res.data.skills,
+          power: res.data.power
+        });
+      });
+    axios
+      .get(`http://localhost:5050/eggs/${this.getRandomInt(2)}`)
+      .then(res => {
+        this.setState({
+          eggs: res.data.image
+        });
+      });
+  }
 
-    render() {
+  render() {
+    return (
+      <div className="cards">
+        <Flippy
+          flipOnClick={true} // default false
+          flipDirection="horizontal" // horizontal or vertical
+          ref={r => (this.flippy = r)} // to use toggle method like this.flippy.toggle()
+          // if you pass isFlipped prop component will be controlled component.
+          // and other props, which will go to div
+        >
+          <FrontSide className="imageFront">
+            <Front src={this.state.eggs} />
+          </FrontSide>
 
-	    return (
-            <div className="cards">
-
-            <Flippy
-                flipOnClick={true} // default false
-                flipDirection="horizontal" // horizontal or vertical
-                ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
-                // if you pass isFlipped prop component will be controlled component.
-                // and other props, which will go to div
-            >
-                <FrontSide className="imageFront">
-                <Front src={this.state.eggs} />
-                </FrontSide>
-
-                <BackSide className="imageFront">
-                    <Back 
-                    character= {this.state.character}
-                    name={this.state.name}
-                    skills={this.state.skills}
-                     />
-                </BackSide>
-            </Flippy>
-            
-            </div>
-        )
-    }
+          <BackSide className="imageFront">
+            <Back
+              character={this.state.character}
+              name={this.state.name}
+              skills={this.state.skills}
+              power={this.state.power}
+            />
+          </BackSide>
+        </Flippy>
+      </div>
+    );
+  }
 }
 
 export default Cards;
